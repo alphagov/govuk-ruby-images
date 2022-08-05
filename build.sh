c68a8320bae058b6fce5e5af615ba2226415a0a0
Sam Simpson <sam.simpson@digital.cabinet-office.gov.uk>
2022-08-05 11:07:10 +0100

    Update build script to push patch version tags

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for VERSION in versions/*; do
+  RUBY_IS_PATCH=false
   source $VERSION
   echo "Building image for Ruby ${RUBY_MAJOR} (${RUBY_VERSION})"
   # Build & push base image
@@ -9,11 +10,19 @@ for VERSION in versions/*; do
     --build-arg "RUBY_MAJOR=${RUBY_MAJOR}" \
     --build-arg "RUBY_VERSION=${RUBY_VERSION}" \
     --build-arg "RUBY_DOWNLOAD_SHA256=${RUBY_DOWNLOAD_SHA256}"
+  docker tag "ghcr.io/alphagov/govuk-ruby-base:${RUBY_MAJOR}" "ghcr.io/alphagov/govuk-ruby-base:${RUBY_VERSION}"
+  if [ "${RUBY_IS_PATCH}" != "true" ]; then
+    docker push "ghcr.io/alphagov/govuk-ruby-base:${RUBY_MAJOR}"
+  fi
   docker push "ghcr.io/alphagov/govuk-ruby-base:${RUBY_MAJOR}"
 
   # Build & push builder image
   docker build . -t "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_MAJOR}" \
     -f builder.Dockerfile \
     --build-arg "RUBY_MAJOR=${RUBY_MAJOR}"
-  docker push "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_MAJOR}"
+  docker tag "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_MAJOR}" "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_VERSION}"
+  if [ "${RUBY_IS_PATCH}" != "true" ]; then
+    docker push "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_MAJOR}"
+  fi
+  docker push "ghcr.io/alphagov/govuk-ruby-builder:${RUBY_VERSION}"
 done
