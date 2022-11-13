c2968245abc3cf3178df9f240371fa34c6221df7
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-11-13 19:55:17 +0000

    Run builds in parallel and update `docker/login-action`.

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #
-# To run without pushing to DockerHub, set DRY_RUN to anything other than the
-# empty string. For example:
+# To build without pushing to a registry, set DRY_RUN to anything other than
+# the empty string. For example:
 #
 #     DRY_RUN=1 ./build.sh
+#
+# To build and push images for just one Ruby version instead of all versions in
+# versions/*, specify a version filename from versions/*. For example:
+#
+#     ./build.sh 3_1
 #
 
 set -eu
 
-for VERSION in versions/*; do
+# Build (and push, unless $DRY_RUN is non-empty) the base and builder images
+# for the Ruby version defined in the file versions/$1.
+build_version() {
   RUBY_IS_PATCH=false
-  source "${VERSION}"
+  source "$1"
 
   for IMAGE_TYPE in base builder; do
     echo "Building ${IMAGE_TYPE} image for Ruby ${RUBY_MAJOR} (${RUBY_VERSION})"
@@ -26,7 +33,7 @@ for VERSION in versions/*; do
       "ghcr.io/alphagov/${IMAGE_NAME}:${RUBY_VERSION}"
 
     if [[ -n ${DRY_RUN:-} ]]; then
-      echo "DRY_RUN is set so not pushing to DockerHub"
+      echo "dry run: not pushing image to registry"
     else
       if [[ "${RUBY_IS_PATCH}" != "true" ]]; then
         docker push "ghcr.io/alphagov/${IMAGE_NAME}:${RUBY_MAJOR}"
@@ -34,4 +41,12 @@ for VERSION in versions/*; do
       docker push "ghcr.io/alphagov/${IMAGE_NAME}:${RUBY_VERSION}"
     fi
   done
-done
+}
+
+if [[ -n "${1:-}" ]]; then
+  build_version "$1"
+else
+  for v in versions/*; do
+    build_version "$v"
+  done
+fi
